921ff75a25416be2c3c1b0fc0e7f38fb5b704759
BenjaminArhancet <barhancet@gaming.tech>
2023-12-05 20:43:37 +0100

    init

---
+++ config.js
@@ -0,0 +1,3 @@
+export const publicToken = "public_uYyIEKMnm6djRmtA";
+export const mainSceneUUID = "a97b85a5-986d-4cc2-9b60-7016fd96f999";
+export const characterControllerSceneUUID = "0a487e3d-6265-4ee6-9fa1-8b8b28433a68";
