10cffb1f5e266ad3bcbe74eea8c2c0280eb90bf9
BenjaminArhancet <barhancet@gaming.tech>
2023-12-09 21:30:35 +0100

    oue

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-export const publicToken = "public_uYyIEKMnm6djRmtA";
-export const mainSceneUUID = "a97b85a5-986d-4cc2-9b60-7016fd96f999";
-export const characterControllerSceneUUID = "0a487e3d-6265-4ee6-9fa1-8b8b28433a68";
-export const mainCamera = "75a46ad9-45eb-489c-a003-261572fb2e36";
+export const publicToken = "public_VooD9rXrjcy5Pd1u";
+export const mainSceneUUID = "cd6d507c-e3b7-4587-8f0a-f22d0606644c";
+export const characterControllerSceneUUID = "16c91da5-e41d-4635-ba0c-a6e75168c7ed";
