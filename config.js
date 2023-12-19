e8e25abec721783fed77d8e2e062b6bee5723699
BenjaminArhancet <barhancet@gaming.tech>
2023-12-19 14:26:30 +0100

    label bon

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 export const publicToken = "public_VooD9rXrjcy5Pd1u";
 export const mainSceneUUID = "cd6d507c-e3b7-4587-8f0a-f22d0606644c";
 export const characterControllerSceneUUID = "16c91da5-e41d-4635-ba0c-a6e75168c7ed";
-export const switch1 = "c8464aca-ee5d-401b-b334-a1ed029a45da";
 
 export const door1 = "381b7a5c-8cb9-4cda-8838-806428ee868b";
 export const CdoubleDoorElevator = "cf9971fd-e0d4-4168-a5cf-6e6b3ae5826b";
